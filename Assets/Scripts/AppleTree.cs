ba956a469d0c0e1cd4f8957368d1df5eb6a68c24
peterduong968 <54965701+peterduong968@users.noreply.github.com>
2019-09-26 00:10:17 -0400

    Step 3. Added Basic Movement to Apple Tree

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    //prefab for instantiating apples
+    public GameObject applePrefab;
+
+    //speed at which apple tree moves
+    public float speed = 1f;
+
+    //Distance where apple tree turns around
+    public float leftAndRightEdge = 10f;
+
+    //Chance the apple tree will change direction
+    public float chanceToChangeDirections = .1f;
+
+    //rate at which apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+    void Start()
+    {
+        
+    }
+
+   void Update()
+    {
+        //basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
