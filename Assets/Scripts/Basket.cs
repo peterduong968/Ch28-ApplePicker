3cbc135d4b6ae579a5d9206cfd576ef5cfc35f67
peterduong968 <54965701+peterduong968@users.noreply.github.com>
2019-09-27 15:53:12 -0400

    Step 5. Added text elements and counting  system

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Set Dynamically")]
+    public Text scoreGT;
     void Start()
     {
-        
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
     }
 
     // Update is called once per frame
@@ -26,5 +30,19 @@ public class Basket : MonoBehaviour
         Vector3 pos = this.transform.position;
         pos.x = mousePos3D.x;
         this.transform.position = pos;
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        //Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+            int score = int.Parse(scoreGT.text);
+            score += 100;
+            scoreGT.text = score.ToString();
+        }
+        
     }
 }
